5d0b38752dfdc1d34c2d369a3fb1229a49b07286
Victor Trejo <vic3jo@gmail.com>
2020-11-10 14:07:36 -0400

    Filtering stopwords

--- models/restaurant_reviews_prediction/restaurant_reviews_prediction_base_model.py
+++ models/restaurant_reviews_prediction/restaurant_reviews_prediction_base_model.py
@@ -18,6 +18,8 @@ class RestaurantReviewsPredictionModel:
     # File of the dataset used to train the model
     __dataset_filename = 'dataset.tsv'
 
+    __stop_words = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
+
     __negative_words_contractions = {
         'isn\'t': 'is not',
         'aren\'t': 'are not',
@@ -108,11 +110,13 @@ class RestaurantReviewsPredictionModel:
 
         text = re.sub('not ([a-z]+)', r'not_\1', text)
 
-        # Remove stop words that are not negative
-        # TODO
     
         # Apply stemming
         tokens = text.split()
+        
+        # Remove stop words that are not negative
+        tokens = [word for word in tokens if word not in self.__stop_words]
+
         tokens = [self.__stemmer.stem(token) for token in tokens]
 
         return ' '.join(tokens)
